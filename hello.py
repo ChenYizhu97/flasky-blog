410a1969f3629addae954df06dd129a6647571d7
TheBestKola <cyzhaa@163.com>
2018-08-09 02:07:25 +0800

    chapter 3

--- hello.py
+++ hello.py
@@ -1,21 +1,32 @@
-from flask import Flask, request, make_response, redirect, url_for, abort 
+from flask import Flask, render_template 
 from flask_script import Manager
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 manager = Manager(app)
 
 
 @app.route('/')
 def index():
-    res = make_response('<h1>this response carries a cookie!<h1>')
-    res.set_cookie('answer', '42')
-    user_agent = request.headers.get('User-Agent')
-    return redirect(url_for('user',name='a'))
+    return render_template('index.jinja', current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
 def user(name):
-    abort(404)
-    return '<h1>hello world! %s<h1>' % name
+    return render_template('user.jinja', name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.jinja'), 400
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.jinja'), 500
 
 
 if __name__ == '__main__':
